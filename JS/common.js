eb1121102fe9e49923a98655acb1392c44d14552
TommyGun1510 <bytomas135@gmail.com>
2024-07-11 18:51:28 -0300

    $$$
    Co-Authored-By: FelipeGil05 <143405071+FelipeGil05@users.noreply.github.com>
    Co-Authored-By: Sergio Duarte <143446600+RomanDuarte@users.noreply.github.com>
    Co-Authored-By: Mateo Fux <106816941+Mateof311@users.noreply.github.com>

--- JS/common.js
+++ JS/common.js
@@ -1,7 +1,7 @@
 fetch("https://dolarapi.com/v1/dolares")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
         localStorage.setItem('DOLARES', JSON.stringify(data))
     })
     .catch((error) => {
@@ -16,70 +16,70 @@ dolar_H_0 = document.querySelector('.moneda-0 h3');
 dolar_H_0.innerHTML = "Dolar " + MONEDAS[0].nombre;
 
 dolar_C_0 = document.querySelector('.moneda-0 .compra .plata');
-dolar_C_0.textContent = MONEDAS[0].compra;
+dolar_C_0.textContent = "$" + MONEDAS[0].compra;
 
 dolar_V_0 = document.querySelector('.moneda-0 .venta .plata');
-dolar_V_0.innerHTML = MONEDAS[0].venta;
+dolar_V_0.innerHTML = "$" + MONEDAS[0].venta;
 
 // dolar blue
 dolar_H_1 = document.querySelector('.moneda-1 h3');
 dolar_H_1.innerHTML = "Dolar " + MONEDAS[1].nombre;
 
 dolar_C_1 = document.querySelector('.moneda-1 .compra .plata');
-dolar_C_1.textContent = MONEDAS[1].compra;
+dolar_C_1.textContent = "$" + MONEDAS[1].compra;
 
 dolar_V_1 = document.querySelector('.moneda-1 .venta .plata');
-dolar_V_1.innerHTML = MONEDAS[1].venta;
+dolar_V_1.innerHTML = "$" + MONEDAS[1].venta;
 
 // dolar bolsa
 dolar_H_2 = document.querySelector('.moneda-2 h3');
 dolar_H_2.innerHTML = "Dolar " + MONEDAS[2].nombre;
 
 dolar_C_2 = document.querySelector('.moneda-2 .compra .plata');
-dolar_C_2.textContent = MONEDAS[2].compra;
+dolar_C_2.textContent = "$" + MONEDAS[2].compra;
 
 dolar_V_2 = document.querySelector('.moneda-2 .venta .plata');
-dolar_V_2.innerHTML = MONEDAS[2].venta;
+dolar_V_2.innerHTML = "$" + MONEDAS[2].venta;
 
 // dolar contado con liquidacion
 dolar_H_3 = document.querySelector('.moneda-3 h3');
 dolar_H_3.innerHTML = "Dolar " + MONEDAS[3].nombre;
 
 dolar_C_3 = document.querySelector('.moneda-3 .compra .plata');
-dolar_C_3.textContent = MONEDAS[3].compra;
+dolar_C_3.textContent = "$" + MONEDAS[3].compra;
 
 dolar_V_3 = document.querySelector('.moneda-3 .venta .plata');
-dolar_V_3.innerHTML = MONEDAS[3].venta;
+dolar_V_3.innerHTML = "$" + MONEDAS[3].venta;
 
 // dolar mayorista
 dolar_H_4 = document.querySelector('.moneda-4 h3');
 dolar_H_4.innerHTML = "Dolar " + MONEDAS[4].nombre;
 
 dolar_C_4 = document.querySelector('.moneda-4 .compra .plata');
-dolar_C_4.textContent = MONEDAS[4].compra;
+dolar_C_4.textContent = "$" + MONEDAS[4].compra;
 
 dolar_V_4 = document.querySelector('.moneda-4 .venta .plata');
-dolar_V_4.innerHTML = MONEDAS[4].venta;
+dolar_V_4.innerHTML = "$" + MONEDAS[4].venta;
 
 // dolar cryto
 dolar_H_5 = document.querySelector('.moneda-5 h3');
 dolar_H_5.innerHTML = "Dolar " + MONEDAS[5].nombre;
 
 dolar_C_5 = document.querySelector('.moneda-5 .compra .plata');
-dolar_C_5.textContent = MONEDAS[5].compra;
+dolar_C_5.textContent = "$" + MONEDAS[5].compra;
 
 dolar_V_5 = document.querySelector('.moneda-5 .venta .plata');
-dolar_V_5.innerHTML = MONEDAS[5].venta;
+dolar_V_5.innerHTML = "$" + MONEDAS[5].venta;
 
 // dolar tarjeta
 dolar_H_6 = document.querySelector('.moneda-6 h3');
 dolar_H_6.innerHTML = "Dolar " + MONEDAS[6].nombre;
 
 dolar_C_6 = document.querySelector('.moneda-6 .compra .plata');
-dolar_C_6.textContent = MONEDAS[6].compra;
+dolar_C_6.textContent = "$" + MONEDAS[6].compra;
 
 dolar_V_6 = document.querySelector('.moneda-6 .venta .plata');
-dolar_V_6.innerHTML = MONEDAS[6].venta;
+dolar_V_6.innerHTML = "$" + MONEDAS[6].venta;
 
 // FECHA Y HORA DE ACTUALIZACION
 
@@ -303,6 +303,8 @@ function boton6() {
     localStorage.setItem('FAVORITOS', JSON.stringify(FAVORITOS));
 }
 
+
+// LABEL SELECT
 const tarjetas = document.querySelectorAll('.cotizaciones .tarjeta');
 const tarjeta0 = document.querySelector('.cotizaciones .tarjeta0');
 const tarjeta1 = document.querySelector('.cotizaciones .tarjeta1');
